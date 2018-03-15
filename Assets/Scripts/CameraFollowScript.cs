ec4fa6e85702b37ae1034da4bd5b14a7a4fab251
avms25 <avms25@hotmail.com>
2018-03-15 18:10:19 -0400

    Observer_Mode
    Added second drone to test observer mode and minimap implementation into
    the UI.

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	private Transform ourDrone;
+	void Awake(){
+		ourDrone = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	private Vector3 velocityCameraFollow;
+	public Vector3 behindPosition = new Vector3(0,0,0);
+	public float angle;
+	void FixedUpdate(){
+		transform.position = Vector3.SmoothDamp (transform.position, ourDrone.transform.TransformPoint (behindPosition) + Vector3.up * Input.GetAxis ("Vertical"), ref velocityCameraFollow, 0.1f);
+		transform.rotation = Quaternion.Euler (new Vector3 (angle, ourDrone.GetComponent<DroneMovementScript> ().currentYRotation, 0));
+	}
+}
---
+++ Assets/Scripts/basicAI.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+	public class basicAI : MonoBehaviour {
+
+		public UnityEngine.AI.NavMeshAgent agent;
+		public ThirdPersonCharacter character;
+
+		public enum State{
+			PATROL,
+			CHASE
+		}
+
+		public State state;
+		private bool alive;
+
+		// Variables for Patrolling
+		public GameObject[] waypoints;
+		private int waypointInd = 0;
+		public float patrolSpeed = 0.5f;
+
+		//Variables for Chase
+		public float chaseSpeed = 1f;
+		public GameObject target;
+
+		// Use this for initialization
+		void Start () {
+			agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+			character = GetComponent<ThirdPersonCharacter> ();
+
+			agent.updatePosition = true;
+			agent.updateRotation = false;
+
+			state = basicAI.State.PATROL;
+
+			alive = true;
+
+			//STAR Finate State Machine
+			StartCoroutine("FSM");
+		}
+
+		IEnumerator FSM()
+		{
+			while (alive) 
+			{
+				switch (state) 
+				{
+				case State.PATROL:
+					Patrol ();
+					break;
+				case State.CHASE:
+					Chase ();
+					break;
+				}
+				yield return null;
+			}
+		}
+
+		void Patrol()
+		{
+			agent.speed = patrolSpeed;
+				if (Vector3.Distance (this.transform.position, waypoints[waypointInd].transform.position) >= 2)
+				{
+					agent.SetDestination(waypoints[waypointInd].transform.position);
+					character.Move(agent.desiredVelocity, false,false);
+					
+
+				}
+				else if (Vector3.Distance(this.transform.position, waypoints[waypointInd].transform.position) <= 2)
+				{
+					waypointInd +=1;
+					if(waypointInd >= waypoints.Length)
+					{
+						waypointInd = 0;
+					}
+				}
+				else
+				{
+					character.Move(Vector3.zero, false, false);
+				}
+		}
+		void Chase()
+		{
+			agent.speed = chaseSpeed;
+			agent.SetDestination (target.transform.position);
+			character.Move (agent.desiredVelocity, false, false);
+		}
+
+		void OnTriggerEnter (Collider coll)
+		{
+			if (coll.tag == "Player")
+			{
+				state = basicAI.State.CHASE;
+				target = coll.gameObject;
+			}
+			
+		}
+	}
+}
