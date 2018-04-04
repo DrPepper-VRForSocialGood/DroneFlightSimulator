bfcac43e87aa54dd8fce3a25330dd3deee2f23ac
samo012 <samole@live.com>
2018-04-04 16:27:13 -0400

    Added Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (Input.GetButtonDown("Fire3"))
+        {
+            Debug.Log("Options");
+                }
+    }
+}
