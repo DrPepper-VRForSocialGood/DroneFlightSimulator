5d14ea454a1115a0153b7af69c4baff49cb03097
avms25 <avms25@hotmail.com>
2018-04-26 16:59:03 -0400

    Minimap
    Added the Google Maps minimap style and the "Dot" on the map to represent the drone, needs tweaking due to scale of building vs reality

---
+++ Assets/MinimapDotMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapDotMovement : MonoBehaviour {
+	public int altitude = 120;
+	public GameObject drone;
+	Vector3 dronePos;
+	Vector3 newPos;
+	Quaternion rot = new Quaternion(0,0,0,0);
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dronePos = drone.transform.position;
+		newPos.Set(dronePos.x, altitude, dronePos.z);
+		gameObject.transform.SetPositionAndRotation (newPos, rot);
+	}
+}
+//27.1 120.9 52.2
